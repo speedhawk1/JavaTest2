9e3107ead226b6d9364d549418b7da7d36282e36
speedhawk1 <1010259701@qq.com>
2016-09-07 20:23:05 +0800

    update

--- src/JavaTest2/JavaTest2_4Calender.java
+++ src/JavaTest2/JavaTest2_4Calender.java
@@ -1,9 +1,5 @@
 package JavaTest2;
 import java.util.Scanner;
-
-/**
- * Created by Administrator on 2016/9/4.
- */
 public class JavaTest2_4Calender {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -14,7 +10,8 @@ public class JavaTest2_4Calender {
         int a = 0;
         int b = 0;
         for (int y = 1900; y < ysum; ++y) {
-            if (ysum % 4 == 0 && ysum % 100 != 0 || ysum%400==0) {
+            if (y % 4 == 0 && y % 100 != 0 || y%400==0) {
+            /*错误一：这里误将y写成ysum,导致循环遍历失效。*/
                 a += 366;
             } else {
                 a += 365;
@@ -47,6 +44,38 @@ public class JavaTest2_4Calender {
             System.out.println("今天是星期日");
         } else {
             System.out.println("今天是星期" + x);
+        }
+        System.out.print("日\t一\t二\t三\t四\t五\t六\n");
+        int wholeDay=a+b+1;
+        int week=wholeDay%7;
+//        以此上2语句可推算出需要的空格数
+        for(int weekDay=0;weekDay<week;++weekDay){
+            System.out.print("\t");
+        }
+        int monthDays;
+        switch(msum){
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                monthDays=30;
+                break;
+            case 2:
+                if(ysum % 4 == 0 && ysum % 100 != 0 || ysum%400==0){
+                monthDays=29;
+        }else{
+                monthDays=28;
+        }
+        break;
+            default:
+                monthDays=31;
+        }
+        for(int weekDays=1;weekDays<=monthDays;++weekDays){
+           /* 错误二：这里的print全部不带ln，目的是按行输出值。*/
+            System.out.print(weekDays+"\t");
+            if((a+b+weekDays)%7==6){
+                System.out.print("\n");
+            }
         }
     }
 }
